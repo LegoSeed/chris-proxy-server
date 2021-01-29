2e157027fca5ee36a1708d3857eb2632b250776c
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-29 12:46:50 -0700

    Added compression

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+const compression = require('compression');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
+app.use(compression());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
