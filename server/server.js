aa865ecd073a557ab55325f5c6f62559cbd5801d
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-29 18:00:07 -0700

    Went on a diet

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-const compression = require('compression');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(compression());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
